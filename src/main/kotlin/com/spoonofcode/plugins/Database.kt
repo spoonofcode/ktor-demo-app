4fcf196d5906c48b325ca04c1c8285ca6ae2a407
Luczak Bartosz <spoonofcode@gmail.com>
2024-07-10 16:13:09 +0200

    #2 Add example data on start

--- src/main/kotlin/com/spoonofcode/plugins/Database.kt
+++ src/main/kotlin/com/spoonofcode/plugins/Database.kt
@@ -1,5 +1,6 @@
 package com.spoonofcode.plugins
 
+import com.spoonofcode.data.model.TaskCategories
 import com.spoonofcode.data.model.Tasks
 import com.spoonofcode.data.model.Users
 import com.spoonofcode.data.model.updateTaskTrigger
@@ -9,7 +10,7 @@ import io.ktor.server.application.*
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -18,9 +19,10 @@ fun Application.configureDatabases() {
     val jdbcUrl = environment.config.property("storage.jdbcURL").getString()
     val db = Database.connect(provideDataSource(jdbcUrl, driverClass))
     transaction(db) {
+        dropTables()
         SchemaUtils.create(Users, Tasks)
         updateTaskTrigger()
-        deleteAllData()
+        setExampleData()
     }
 }
 
@@ -36,9 +38,63 @@ private fun provideDataSource(url: String, driverClass: String): HikariDataSourc
     return HikariDataSource(hikariConfig)
 }
 
-private fun deleteAllData() {
-    Tasks.deleteAll()
-    Users.deleteAll()
+private fun dropTables() {
+    transaction {
+        SchemaUtils.drop(Users, Tasks, TaskCategories) // Add all the tables you want to drop here
+    }
+}
+
+private fun setExampleData() {
+    Users.insert {
+        it[firstName] = "Leo"
+        it[lastName] = "Messi"
+    }
+
+    Users.insert {
+        it[firstName] = "Christiano"
+        it[lastName] = "Ronaldo"
+    }
+
+    Users.insert {
+        it[firstName] = "Frank"
+        it[lastName] = "Lampard"
+    }
+
+    TaskCategories.insert {
+        it[name] = "Sport"
+    }
+
+    TaskCategories.insert {
+        it[name] = "Work"
+    }
+
+    Tasks.insert {
+        it[description] = "Example Task 1"
+        it[isCompleted] = false
+        it[categoryId] = 1
+        it[userId] = 1
+    }
+
+    Tasks.insert {
+        it[description] = "Example Task 2"
+        it[isCompleted] = false
+        it[categoryId] = 2
+        it[userId] = 1
+    }
+
+    Tasks.insert {
+        it[description] = "Example Task 3"
+        it[isCompleted] = false
+        it[categoryId] = 1
+        it[userId] = 2
+    }
+
+    Tasks.insert {
+        it[description] = "Example Task 4"
+        it[isCompleted] = false
+        it[categoryId] = 2
+        it[userId] = 2
+    }
 }
 
 suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO) { block() }
